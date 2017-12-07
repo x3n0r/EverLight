0ee23c05d1eb977e1bb361d807ef8da4e8befd7d
x3n0r <33955757+x3n0r@users.noreply.github.com>
2017-12-07 14:35:26 +0100

    First Commit

---
+++ EverLight/Assets/Scripts/Interactable/Interactable Objects/SignPost.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SignPost : ActionItem {
+    public string[] dialogue;
+    public override void Interact()
+    {
+        DialogueSystem.Instance.AddNewDialogue(dialogue, "Sign");
+    }
+}
---
+++ EverLight/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class CameraController : MonoBehaviour
+{
+    public float PlayerCameraDistance { get; set; }
+    public Transform cameraTarget;
+
+    Camera playerCamera;
+    float zoomSpeed = 35f;
+
+    void Start()
+    {
+        PlayerCameraDistance = 10f;
+        playerCamera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.GetAxisRaw("Mouse ScrollWheel") != 0)
+        {
+            float scroll = Input.GetAxis("Mouse ScrollWheel");
+            playerCamera.fieldOfView -= scroll * zoomSpeed;
+            if (playerCamera.fieldOfView > 80f)
+                playerCamera.fieldOfView = 80f;
+            if (playerCamera.fieldOfView < 45f)
+                playerCamera.fieldOfView = 45f;
+            playerCamera.fieldOfView = Mathf.Clamp(playerCamera.fieldOfView, 0, 180);
+        }
+    }
+}
